7a4ccc688f81ca68bf098aa09835e9a105cefe83
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-07-22 10:35:49 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num=3;
+if(num>0)
+{
+printf("the number is positive");
+}
+elseif(num<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+return0;
+}
